aa812d624b734ef46638ee761fd952443c378454
UFifty50sh <UFifty50.github@gmail.com>
2024-07-02 22:06:43 +0100

    addedUI template

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,6 @@
-import { GiftCard } from './components/gift-card/gift-card';
 import styles from './App.module.scss';
 
 function App() {
-    return (
-        <div className={styles.App}>
-            <GiftCard />
-            <GiftCard full />
-        </div>
-    );
+    return <div className={styles.App}></div>;
 }
 export default App;
